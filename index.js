22284a9a2f075515c3d3ece709102f429d148d0c
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-05-06 10:03:06 +0600

    working on index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-//pushing environment to git
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app =express();
+const port= process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('running server')
+})
+
+app.listen(port, ()=>{
+    console.log('listening to port', port);
+})
