267a93f00b310032073281a2b8c913f263b63cdf
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-05-08 22:26:10 +0600

    some

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const product = await productCollection.findOne(query);
+            //some
             res.send(product);
         })
 
