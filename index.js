7227819d40858dab5b288c329fae10f96be9d424
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-05-09 20:00:00 +0600

    failed to implement JWT

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -18,6 +19,16 @@ async function run() {
     try {
         await client.connect();
         const productCollection = client.db('inventory_db').collection('products');
+
+        //AUTH
+        app.get('/login', async(req, res) =>{
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({accessToken});
+        })
+
 
         app.get('/product', async (req, res) => {
             const query = {};
